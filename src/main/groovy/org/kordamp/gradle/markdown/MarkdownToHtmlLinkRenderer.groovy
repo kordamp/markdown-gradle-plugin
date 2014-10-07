4eff99f3a4036b5b343384c8dfdb3daf41b54149
Ed MacDonald <edmacdonald555@gmail.com>
2014-10-07 15:37:47 -0400

    Fixed codenarc errors (oops)

--- src/main/groovy/org/kordamp/gradle/markdown/MarkdownToHtmlLinkRenderer.groovy
+++ src/main/groovy/org/kordamp/gradle/markdown/MarkdownToHtmlLinkRenderer.groovy
@@ -12,35 +12,35 @@ import static org.pegdown.LinkRenderer.Rendering
  * @author Ed MacDonald
  */
 class MarkdownToHtmlLinkRenderer extends LinkRenderer {
-    private Conversion conversion = Conversion.MARKDOWN
+    private final Conversion conversion = Conversion.MARKDOWN
 
     @Override
     Rendering render(AutoLinkNode node) {
-        return adjustHref(super.render(node))
+        adjustHref(super.render(node))
     }
 
     @Override
     Rendering render(ExpLinkNode node, String text) {
-        return adjustHref(super.render(node, text))
+        adjustHref(super.render(node, text))
     }
 
     @Override
     Rendering render(RefLinkNode node, String url, String title, String text) {
-        return adjustHref(super.render(node, url, title, text))
+        adjustHref(super.render(node, url, title, text))
     }
 
     @Override
     Rendering render(WikiLinkNode node) {
-        return adjustHref(super.render(node))
+        adjustHref(super.render(node))
     }
 
     // Assumes each extension starts with "."
     private Rendering adjustHref( Rendering rendering ) {
         if(conversion.extensions().size() > 0) {
-            def regex =  conversion.extensions().join("|\\")
-            regex = "(\\" + regex + ")\$"
+            def regex =  conversion.extensions().join('|\\')
+            regex = '(\\' + regex + ')\$'
             rendering.href = rendering.href.replaceFirst(regex, conversion.targetExtension())
         }
-        return rendering
+        rendering
     }
 }
